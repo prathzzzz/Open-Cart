4d77c56ee8c94333d35b30025a087e7358076b4d
prathzzzz <kariapratham@gmail.com>
2024-12-06 19:44:01 +0530

    chore : implement cross-browser testing setup and add logging in account registration test

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,12 +1,20 @@
 package testBase;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 /**
  * Base class for all test cases
@@ -14,17 +22,28 @@ import java.time.Duration;
  */
 public abstract class BaseClass {
     protected WebDriver driver;
-
+    public Logger logger;
+    public Properties properties;
     /**
      * Setup method to initialize browser before each test class
      */
     @BeforeClass
-    public void setup() {
-        driver = new ChromeDriver();
+    @Parameters({"os","browser"})
+    public void setup(String os, String browser) throws IOException {
+        FileReader fileReader = new FileReader("./src//test//resources//config.properties");
+        properties = new Properties();
+        properties.load(fileReader);
+        logger = LogManager.getLogger(this.getClass());
+        switch (browser.toLowerCase()) {
+            case "chrome" : driver = new ChromeDriver(); break;
+            case "edge" : driver = new EdgeDriver(); break;
+            default :
+                System.out.println("Invalid browser name"); return;
+        }
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        driver.get("http://localhost/opencart/upload/");
+        driver.get(properties.getProperty("appURL"));
     }
 
     /**
--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -21,27 +21,47 @@ public class TC001_AccountRegistrationTest extends BaseClass {
      */
     @Test
     public void verifyAccountRegistration() {
-        // Initialize HomePage and navigate to registration
-        HomePage homePage = new HomePage(driver);
-        homePage.clickMyAccount();
-        homePage.clickRegister();
+        logger.info("Starting TC001_AccountRegistrationTest");
 
-        // Fill registration form with random data
-        AccountRegistrationPage accountRegistrationPage = new AccountRegistrationPage(driver);
-        accountRegistrationPage.setFirstName(generateRandomString());
-        accountRegistrationPage.setLastName(generateRandomString());
-        accountRegistrationPage.setEmail(generateRandomString() + "@gmail.com");
-        accountRegistrationPage.setTelephone(generateRandomNumber());
-        accountRegistrationPage.setPassword(generateRandomString() + "@" + generateRandomNumber() + generateRandomString());
-        accountRegistrationPage.setPasswordConfirm();
+        try {
+            HomePage homePage = new HomePage(driver);
+            homePage.clickMyAccount();
+            logger.info("Clicked on MyAccount Link");
+            homePage.clickRegister();
+            logger.info("Clicked on Register Link");
 
-        // Submit registration
-        accountRegistrationPage.clickCheckboxAgree();
-        accountRegistrationPage.clickContinueButton();
+            // Fill registration form with random data
+            logger.info("Providing Customer Details");
+            AccountRegistrationPage accountRegistrationPage = new AccountRegistrationPage(driver);
+            accountRegistrationPage.setFirstName(generateRandomString());
+            accountRegistrationPage.setLastName(generateRandomString());
+            accountRegistrationPage.setEmail(generateRandomString() + "@gmail.com");
+            accountRegistrationPage.setTelephone(generateRandomNumber());
+            accountRegistrationPage.setPassword(generateRandomString() + "@" + generateRandomNumber() + generateRandomString());
+            accountRegistrationPage.setPasswordConfirm();
 
-        // Verify successful registration
-        String successfulRegistrationMessage = AccountRegistrationPage.SUCCESSFUL_REGISTRATION_MESSAGE;
-        Assert.assertEquals(accountRegistrationPage.getAccountCreatedMessage(), successfulRegistrationMessage);
+            // Submit registration
+            logger.info("Clicked Checkbox");
+            accountRegistrationPage.clickCheckboxAgree();
+            logger.info("Clicked Continue Button");
+            accountRegistrationPage.clickContinueButton();
+
+            // Verify successful registration
+            String successfulRegistrationMessage = AccountRegistrationPage.SUCCESSFUL_REGISTRATION_MESSAGE;
+            logger.info("Validating Expected Message");
+            String actualMessage = accountRegistrationPage.getAccountCreatedMessage();
+            logger.info("Actual message: " + actualMessage);
+            if(actualMessage.equals(successfulRegistrationMessage)) {
+                Assert.assertTrue(true);
+            }else {
+                logger.error("Test failed: ");
+                Assert.fail();
+            }
+        } catch (Exception e) {
+            Assert.fail();
+        }
+        logger.info("Finished TC001_AccountRegistrationTest");
+
     }
 
 }
