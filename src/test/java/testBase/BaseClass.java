102675432f87606f3da7f477725171338833db0c
prathzzzz <kariapratham@gmail.com>
2024-12-07 22:04:47 +0530

    feat: integrate ExtentReports for enhanced test reporting and add screenshot capture functionality

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,8 @@ package testBase;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,17 +14,20 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
 /**
  * Base class for all test cases
  * Handles test setup, teardown, and common utilities
  */
-public abstract class BaseClass {
-    protected WebDriver driver;
+public class BaseClass {
+    protected static WebDriver driver;
     public Logger logger;
     public Properties properties;
     protected ChromeOptions chromeOptions;
@@ -89,5 +94,20 @@ public abstract class BaseClass {
      */
     public String generateRandomNumber() {
         return RandomStringUtils.randomNumeric(10);
+    }
+    public String captureScreen(String tname) throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+        File targetFile=new File(targetFilePath);
+
+        sourceFile.renameTo(targetFile);
+
+        return targetFilePath;
+
     }
 }
