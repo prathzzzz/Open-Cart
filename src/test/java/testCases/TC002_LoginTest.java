2cbd1149ad9f6ba57e657e3fa2156ba8332a8b12
prathzzzz <kariapratham@gmail.com>
2024-12-07 22:05:00 +0530

    refactor: enhance login test with error handling and logging improvements

--- src/test/java/testCases/TC002_LoginTest.java
+++ src/test/java/testCases/TC002_LoginTest.java
@@ -11,32 +11,39 @@ public class TC002_LoginTest extends BaseClass {
     @Test
     public void verifyLogin() {
         logger.info("*****Starting TC002_LoginTest*****");
-        
+
         MyAccountPage myAccountPage = new MyAccountPage(driver);
         LoginPage loginPage = new LoginPage(driver);
-        
+
         logger.info("Clicked My Account");
         myAccountPage.clickMyAccount();
-        
+
         logger.info("Click Login Link");
         loginPage.clickLogin();
-        
+
         logger.info("Entered Email");
         loginPage.setTxtEmail(properties.getProperty("email"));
-        
+
         logger.info("Entered Password");
         loginPage.setTxtPassword(properties.getProperty("password"));
-        
+
         logger.info("Submitted the form");
         loginPage.clickLoginBtn();
-        
-        if (myAccountPage.getMyAccountText().equals(MyAccountPage.SUCCESSFUL_LOGIN_MESSAGE)) {
-            Assert.assertTrue(true);
-        } else {
-            logger.error("Test Failed");
-            Assert.fail();
+
+        try {
+            String errorMessage = loginPage.getIncorrectCredentialMessage();
+            logger.error("Login failed - Incorrect credentials");
+            Assert.fail("Login failed due to incorrect credentials");
+        } catch (Exception e) {
+            if (myAccountPage.getMyAccountText().equals(MyAccountPage.SUCCESSFUL_LOGIN_MESSAGE)) {
+                logger.info("Login successful");
+                Assert.assertTrue(true);
+            } else {
+                logger.error("Test Failed - Unexpected state");
+                Assert.fail("Test failed - Login neither successful nor showed error message");
+            }
         }
-        
+
         logger.info("*****Finished TC002_LoginTest*****");
     }
 }
